d9dcf55e19fb22f33547fc0843ee70505c5316b3
Joe Chirchirillo <cchirchirillo3@gmail.com>
2024-09-27 21:31:22 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './Form';
+import Ideas from './Ideas';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const dummyIdeas = [
+        {id: 1, title: 'Pass Turing',
+        description: 'Study azz off.'
+          },
+        {id: 2, title: 'Make Espresso',
+          description: 'Get jitters, code faster.'
+          },
+        {id: 3, title: 'Prank War',
+          description: 'Googly eye everything.'
+          }
+      ]
+  const [ideas, setIdeas] = useState(dummyIdeas)
+  
+  function addIdea (newIdea) {
+    setIdeas([...ideas, newIdea])
+  }
+  function deleteIdea(id){
+    console.log(id);
+    const filteredIdeas = ideas.filter(idea => idea.id !== id)
+    setIdeas(filteredIdeas)
+  }
+
+  return(
+    <main className='App'>
+        <h1>IdeaBox</h1>
+        <p>Hi!</p>
+        <Form addIdea={addIdea}/>
+        <Ideas ideas={ideas} deleteIdea={deleteIdea}/>
+    </main>
+  )
 }
 
 export default App;
+
+
+// import './App.css';
+// import Form from './Form'
+// import Ideas from './Ideas';
+// import { useState } from 'react';
+
+// function App() {
+//   const [ideas, setIdeas] = useState([]);
+
+//   function addIdea(newIdea) {
+//     setIdeas([...ideas, newIdea]);
+//   }
+
+//   return (
+//     <main className="App">
+//       <h1>IdeaBox</h1>
+//       {/* Conditional rendering if no ideas */}
+//       {!ideas.length && <h2>No ideas yet -- add some!</h2>}
+//       {/* Pass ideas as props to Ideas component */}
+//       <Ideas ideas={ideas} />
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
+// function App() {
+//   const dummyIdeas = [
+//     {id: 1, title: 'Pass Turing',
+//     description: 'Study azz off.'
+//       },
+//     {id: 2, title: 'Make Espresso',
+//       description: 'Get jitters, code faster.'
+//       },
+//     {id: 3, title: 'Prank War',
+//       description: 'Googly eye everything.'
+//       }
+//   ]
+//   const [ideas, setIdeas] = useState(dummyIdeas);
+
+//   return ( 
+//     <main className="App">
+//       <h1>IdeaBox</h1>
+//       {!ideas.length && <h2>No ideas yet -- add some!</h2> }
+//       <Ideas ideas={ideas} />
+//     </main>
+//    )
+// }
+
+
+
+// useState is a React Hook that allows you to 
+// add state to functional components.
+
+// setIdeas is the setter function provided 
+// by useState to modify the value of ideas.
---
+++ src/Form.js
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import './Form.css';
+
+function Form({ addIdea }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  
+  function submitIdeas(event) {
+    event.preventDefault(); 
+    
+    const newIdea = {
+      id: Date.now(),
+      title,
+      description
+    };
+    
+    addIdea(newIdea);
+    
+    clearInput();
+  }
+
+  function clearInput() {
+    setTitle("");  
+    setDescription(""); 
+  }
+
+  return (
+    <form onSubmit={submitIdeas}>
+      <input
+        type='text'
+        placeholder='Title'
+        name='title'
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+
+      <input
+        type='text'
+        placeholder='Description'
+        name='description'
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+
+      <button type="submit">SUBMIT</button>
+    </form>
+  );
+}
+
+export default Form;
+
+
+// import { useState } from "react";
+// import './Form.css';
+
+// function submitIdeas(event) {
+//   event.preventDefault()
+//   const newIdea = {
+//       id: Date.now(),
+//       title,
+//       description
+//   }
+//   addIdea(newIdea)
+//   clearInput()
+// }
+// function clearInput(){
+//   setTitle("")
+//   setDescription("")
+// }
+
+// return (
+//   <form>
+//     <input
+//       type='text'
+//       placeholder='Title'
+//       name='title'
+//       value={title}
+//       onChange={event => setTitle(event.target.value)}
+//     />
+
+//     <input
+//       type='text'
+//       placeholder='Description'
+//       name='description'
+//       value={description}
+//       onChange={event => setDescription(event.target.value)}
+//     />
+
+//     <button onClick = { event => submitIdeas(event)}>SUBMIT</button>
+//   </form>
+// )
+
+// function Form(){
+//   const [title, setTitle] = useState("");
+//   const [description, setDescription] = useState("");
+
+//   return (
+//     <form>
+//       <input
+//         type='text'
+//         placeholder='Title'
+//         name='title'
+//         value={title}
+//         onChange={event => setTitle(event.target.value)}
+//       />
+
+//       <input
+//           type='text'
+//           placeholder='Description'
+//           name='description'
+//           value={description}
+//           onChange={event => setDescription(event.target.value)}
+//         />
+
+//       <button>SUBMIT</button>
+//     </form>
+//   )
+// }
+// export default Form;
